c4a70f841a70d8b0ddc94edfeab02e2328e0a659
Mukul <mukulyadav2302@gmail.com>
2026-06-12 14:18:01 +0530

    removed cors from backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
 
 let notes = [
@@ -28,9 +27,8 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(cors())
-app.use(express.json())
 app.use(requestLogger)
+app.use(express.json())
 app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
@@ -92,5 +90,6 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
