3faef0ed2f59876ad9bfce84755bf6c0f81b9ea4
Mukul <mukulyadav2302@gmail.com>
2026-06-12 20:03:50 +0530

    fixed the error notification

--- part-1/src/App.jsx
+++ part-1/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState('some error happened...')
+  const [errorMessage, setErrorMessage] = useState(null)
   
   useEffect(() => {
     noteService
@@ -54,7 +54,7 @@ const App = () => {
       })
       .catch(error => {
         setErrorMessage(
-          `Note ${note.content} was already removed from server`
+          `The Note "${note.content}" was already removed from server`
         )
         setTimeout(() => {
           setErrorMessage(null)
